e7f7b55e9ec038300360183b59fe2f3669064629
aniket-magare <aniketmagare8609@gmail.com>
2025-04-16 10:49:37 +0530

    created theh front end and add some basic features

---
+++ front-end/src/componets/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+       <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Craving your favorite dishes or essentials delivered right to your doorstep? We've got you covered! With us, enjoy the convenience of exploring a variety of cuisines, grocery items, and more—all from the comfort of your home.</p>
+        <button>View Menu</button>
+       </div>
+      
+    </div>
+  )
+}
+
+export default Header
