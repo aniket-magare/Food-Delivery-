520d6059a097b3d78807782994ff1bc3b3348fd8
aniket-magare <aniketmagare8609@gmail.com>
2025-04-19 11:40:34 +0530

    created basic admin panel and added some basic structure

---
+++ admin/src/pages/List/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './List.css'
+const List = () => {
+  return (
+    <div>
+      test1
+    </div>
+  )
+}
+
+export default List
---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Orders.css'
+
+const Orders = () => {
+  return (
+    <div>
+      test3
+    </div>
+  )
+}
+
+export default Orders
