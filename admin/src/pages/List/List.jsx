690c2f057c2f4d7d6fb20981acf04a5b1fa63b84
aniket-magare <aniketmagare8609@gmail.com>
2025-04-24 23:17:51 +0530

    updated list.jsx file

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import './List.css'
-const List = () => {
-  return (
-    <div>
-      test1
-    </div>
-  )
-}
 
-export default List
+import React, { useEffect } from "react";
+import "./List.css";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+
+
+const List = () => {
+const [list, setList] = useState([]);
+const url = "http://localhost:4000";
+
+
+
+const fetchList = async () => {
+  const response = await axios.get(`${url}/api/food/list`);
+  console.log(response.data);
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+  useEffect(() => {
+    fetchList();
+  }, []);
+  return <div> test1 </div>;
+};
+
+
+
+export default List;
+
