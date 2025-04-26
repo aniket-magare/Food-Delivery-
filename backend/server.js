b691d4e33b3f504a0517d2aa99c936da4faacdd1
aniket-magare <aniketmagare8609@gmail.com>
2025-04-26 23:34:52 +0530

    added user authentication

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express"
 import cors from "cors"
 import { connectDb } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoutes.js"
 
 
 
@@ -21,6 +22,7 @@ connectDb();
 // api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
 
 
 app.get("/",(req,res)=>{
